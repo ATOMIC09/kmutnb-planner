1e4c98f30e96c6b2186cd96ba55300c3079a7887
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-11-19 00:08:38 +0700

    Add sidebar shadcn

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,52 @@
+import { Home, BookOpen } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+
+// Menu items.
+const items = [
+  {
+    title: "วิชาที่เปิดสอน",
+    url: "/",
+    icon: BookOpen,
+  },
+  // {
+  //   title: "test",
+  //   url: "/test",
+  //   icon: Home,
+  // },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="font-LINESeedSansTH_W_Rg">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>เมนู</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon className="w-24 h-24"/>
+                      <span className="text-base">{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
