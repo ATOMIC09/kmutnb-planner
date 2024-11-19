500cf06e9107ed729ea7d1db0fb603ef8b9947e0
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-11-19 23:28:49 +0700

    Add วิทยาเขต, ระดับ, คณะ, ภาควิชา

--- src/components/course-search.tsx
+++ src/components/course-search.tsx
@@ -8,11 +8,22 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
+import { fetchCourses, fetchDepartments } from '@/lib/api';
 
 export default function CourseSearch() {
     const [year, setYear] = useState('');
     const [yearOptions, setYearOptions] = useState<string[]>([]);
     const [semester, setSemester] = useState('1');
+    const [campus, setCampus] = useState('10');
+    const [level, setLevel] = useState('61');
+    const [faculty, setFaculty] = useState('00');
+    const [department, setDepartment] = useState('00');
+    const [departmentList, setDepartmentList] = useState<Department[]>([]);
+
+    interface Department {
+        comboid: string;
+        comboshow: string;
+    }
 
     useEffect(() => {
         // Get the current Gregorian year
@@ -28,22 +39,40 @@ export default function CourseSearch() {
         setYear(options[0]); // Set the first year (current year) as default
     }, []);
 
+    useEffect(() => {
+        const getDepartments = async (facultyId: string) => {
+            try {
+                const departmentsData = await fetchDepartments(facultyId);
+                setDepartmentList(departmentsData);
+                if (facultyId === '0') {
+                    setDepartment('0');
+                } else {
+                    setDepartment(departmentsData[0].comboid);
+                }
+            } catch (error) {
+                console.error('Error fetching departments data:', error);
+            }
+        };
+        getDepartments(faculty);
+    }, [faculty]);
+
+
     return (
-        <main className="font-LINESeedSansTH_W_Rg text-gray-700 px-12 py-4">
+        <main className="font-LINESeedSansTH_W_Rg text-gray-700 px-4 sm:px-12 py-4 w-full">
             <div className="text-4xl">วิชาที่เปิดสอน</div>
-            <div className="p-4 mt-4 mb-4 border-1 rounded-lg shadow-md">
+            <div className="p-4 mt-4 mb-4 border-1 rounded-lg shadow-md w-full min-w-full mx-auto">
                 <div className="mb-4">
-                    <div className="flex gap-8">
-                        <div className="flex-col items-center">
+                    <div className="flex flex-col gap-4">
+                        <div className="flex-col items-center w-full">
                             <label className="block mb-2">ปีการศึกษา</label>
-                            <Select 
-                                value={year} 
+                            <Select
+                                value={year}
                                 onValueChange={(value) => setYear(value)}
                             >
-                                <SelectTrigger className="w-[130px]">
+                                <SelectTrigger className="md:w-[130px]">
                                     <SelectValue defaultValue={year} placeholder="เลือกปีการศึกษา" />
                                 </SelectTrigger>
-                                <SelectContent>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
                                     {yearOptions.map((option) => (
                                         <SelectItem key={option} value={option}>
                                             {option}
@@ -54,23 +83,115 @@ export default function CourseSearch() {
                         </div>
 
                         <div className="flex-col items-center">
-                            <label className="block mb-2">ภาคการศึกษา</label>
-                            <Select 
-                                value={semester} 
+                            <label className="block mb-2">ภาค</label>
+                            <Select
+                                value={semester}
                                 onValueChange={(value) => setSemester(value)}
                             >
-                                <SelectTrigger className="w-[130px]">
+                                <SelectTrigger className="md:w-[130px]">
                                     <SelectValue defaultValue={semester} placeholder="เลือกภาคการศึกษา" />
                                 </SelectTrigger>
-                                <SelectContent>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
                                     <SelectItem value="1">1</SelectItem>
                                     <SelectItem value="2">2</SelectItem>
                                     <SelectItem value="3">3</SelectItem>
                                 </SelectContent>
                             </Select>
                         </div>
+
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">วิทยาเขต</label>
+                            <Select
+                                value={campus}
+                                onValueChange={(value) => setCampus(value)}
+                            >
+                                <SelectTrigger className="md:w-[200px]">
+                                    <SelectValue defaultValue={campus} placeholder="เลือกวิทยาเขต" />
+                                </SelectTrigger>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
+                                    <SelectItem value="10">มจพ. กรุงเทพฯ</SelectItem>
+                                    <SelectItem value="21">มจพ. วิทยาเขตระยอง</SelectItem>
+                                    <SelectItem value="25">มจพ. วิทยาเขตปราจีนบุรี</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">ระดับ</label>
+                            <Select
+                                value={level}
+                                onValueChange={(value) => setLevel(value)}
+                            >
+                                <SelectTrigger className="md:w-[240px]">
+                                    <SelectValue defaultValue={level} placeholder="เลือกระดับ" />
+                                </SelectTrigger>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
+                                    <SelectItem value="41">ประกาศนียบัตรวิชาชีพ</SelectItem>
+                                    <SelectItem value="51">ประกาศนียบัตรวิชาชีพชั้นสูง</SelectItem>
+                                    <SelectItem value="61">ปริญญาตรี 4 ปี / 5 ปี</SelectItem>
+                                    <SelectItem value="62">ปริญญาตรีต่อเนื่อง 2 - 3 ปี</SelectItem>
+                                    <SelectItem value="63">ปริญญาตรีเทียบโอน 2 - 3 ปี</SelectItem>
+                                    <SelectItem value="71">ประกาศนียบัตรบัณฑิต</SelectItem>
+                                    <SelectItem value="81">ปริญญาโท</SelectItem>
+                                    <SelectItem value="91">ปริญญาเอก</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
                     </div>
-                </div>    
+                    <div className="flex justify-between gap-8 py-4">
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">คณะ</label>
+                            <Select
+                                value={faculty}
+                                onValueChange={(value) => setFaculty(value)}
+                            >
+                                <SelectTrigger className="md:w-[300px]">
+                                    <SelectValue defaultValue={faculty} placeholder="เลือกคณะ" />
+                                </SelectTrigger>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
+                                    <SelectItem value="00">ทุกคณะ</SelectItem>
+                                    <SelectItem value="01">คณะวิศวกรรมศาสตร์</SelectItem>
+                                    <SelectItem value="02">คณะครุศาสตร์อุตสาหกรรม</SelectItem>
+                                    <SelectItem value="03">วิทยาลัยเทคโนโลยีอุตสาหกรรม</SelectItem>
+                                    <SelectItem value="04">คณะวิทยาศาสตร์ประยุกต์</SelectItem>
+                                    <SelectItem value="05">คณะอุตสาหกรรมเกษตรดิจิทัล</SelectItem>
+                                    <SelectItem value="06">คณะเทคโนโลยีและการจัดการอุตสาหกรรม</SelectItem>
+                                    <SelectItem value="07">คณะเทคโนโลยีสารสนเทศและนวัตกรรมดิจิทัล</SelectItem>
+                                    <SelectItem value="08">คณะศิลปศาสตร์ประยุกต์</SelectItem>
+                                    <SelectItem value="09">บัณฑิตวิทยาลัยวิศวกรรมศาสตร์นานาชาติฯ</SelectItem>
+                                    <SelectItem value="10">บัณฑิตวิทยาลัย</SelectItem>
+                                    <SelectItem value="11">คณะสถาปัตยกรรมและการออกแบบ</SelectItem>
+                                    <SelectItem value="12">คณะวิศวกรรมศาสตร์และเทคโนโลยี</SelectItem>
+                                    <SelectItem value="13">คณะวิทยาศาสตร์ พลังงานและสิ่งแวดล้อม</SelectItem>
+                                    <SelectItem value="14">คณะบริหารธุรกิจ</SelectItem>
+                                    <SelectItem value="15">วิทยาลัยนานาชาติ</SelectItem>
+                                    <SelectItem value="16">คณะพัฒนาธุรกิจและอุตสาหกรรม</SelectItem>
+                                    <SelectItem value="17">คณะบริหารธุรกิจและอุตสาหกรรมบริการ</SelectItem>
+                                    <SelectItem value="18">อุทยานเทคโนโลยี มจพ.</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">ภาควิชา</label>
+                            <Select
+                                value={department}
+                                onValueChange={(value) => setDepartment(value)}
+                            >
+                                <SelectTrigger className="md:w-[400px]">
+                                    <SelectValue defaultValue={department} placeholder="เลือกภาควิชา" />
+                                </SelectTrigger>
+                                <SelectContent className="font-LINESeedSansTH_W_Rg">
+                                    {departmentList.map((option) => (
+                                        <SelectItem key={option.comboid} value={option.comboid}>
+                                            {option.comboshow}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     );
---
+++ src/lib/api.ts
@@ -0,0 +1,197 @@
+import axios from 'axios';
+import { gunzipSync } from 'zlib';
+import { TOKEN_API_URL, TIMETABLE_API_URL, CLASSINFO_API_URL, LOGIN_API_URL, DEPARTMENT_API_URL } from '@/config';
+import Encryptor from '@/lib/encryption';
+import { jwtDecode } from "jwt-decode";
+
+let token = '';
+
+class V {
+  constructor() {}
+
+  getdateformatora(P: any) {
+    let Z;
+    if (P instanceof Date) {
+      Z = P;
+    } else {
+      Z = new Date(P);
+    }
+    const ce = Z.getDate().toString().padStart(2, '0');
+    const ve = (Z.getMonth() + 1).toString().padStart(2, '0');
+    return Z.getFullYear() + '-' + ve + '-' + ce;
+  }
+}
+
+const Ie = new V();
+
+const getAuthHeader = async () => {
+  token = await getToken();
+  return {
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  };
+};
+
+function decodeBase64Response(base64EncodedString: string) {
+  const decompressed = gunzipSync(Buffer.from(base64EncodedString, 'base64'));
+  return JSON.parse(decompressed.toString());
+}
+
+// Axios instance
+const axiosInstance = axios.create();
+
+axiosInstance.interceptors.response.use(
+  response => response,
+  async error => {
+    if (error.response && error.response.status === 401) {
+      console.log('Got 401 Token expired, fetching new token...');
+      removeStorage('tokenweb');
+      removeStorage('tokentime');
+      const newToken = await fetchNewToken();
+      error.config.headers['Authorization'] = 'Bearer ' + newToken;
+      return axiosInstance.request(error.config);
+    }
+    else if (error.response && error.response.status === 404) {
+      console.error('Got 404', error.response.data);
+      return { status: error.response.status, data: error.response.data };
+    } 
+    else if (error.response && error.response.status === 500) {
+      console.error('Got 500', error.response.data);
+      return { status: error.response.status, data: error.response.data };
+    }
+    else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const userLogin = async (username: string, password: string) => {
+  const encryptor = new Encryptor();
+  const authHeader = await getAuthHeader();
+  const credentials = { username, password };
+  
+  // Transform dates and other necessary values before encryption
+  const transformedCredentials = JSON.stringify(credentials, function (Me, xe) {
+    var Ae = xe;
+    return this[Me] instanceof Date && (Ae = Ie.getdateformatora(xe)), void 0 === this[Me] ? null : Ae;
+  });
+  const encryptedData = encryptor.encryptData(transformedCredentials);
+  const payload = {
+    param: encryptedData
+  };
+  console.log('Send payload:', payload);
+
+  try {
+    const res = await axiosInstance.post(LOGIN_API_URL, payload, authHeader);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const userLogout = async () => {
+  removeStorage('tokenweb');
+  removeStorage('tokentime');
+}
+
+export const fetchTimetable = async (year: number, semester: number) => {
+  const authHeader = await getAuthHeader();
+  const res = await axiosInstance.get(`${TIMETABLE_API_URL}/Timetable/${year}/${semester}`, authHeader);
+  if (res.status !== 200) {
+    return {status: res.status, data: []};
+  }
+  return {status: res.status, data: decodeBase64Response(res.data.result)};
+};
+
+export const fetchTimetableSub = async (year: number, semester: number, startDate: string, endDate: string) => {
+  const authHeader = await getAuthHeader();
+  const res = await axiosInstance.get(`${TIMETABLE_API_URL}/Timetablesub/${year}/${semester}/${startDate}/${endDate}`, authHeader);
+  if (res.status !== 200) {
+    return {status: res.status, data: []};
+  }
+  return {status: res.status, data: decodeBase64Response(res.data.result)};
+};
+
+export const fetchCourses = async (
+  academicYear: number,
+  semester: number,
+  campus: number,
+  level: number,
+  faculty: number,
+  department: number,
+  courseCode: string,
+  courseName: string
+) => {
+  const authHeader = await getAuthHeader();
+
+  if (courseCode === '') {
+    courseCode = '-';
+  }
+  if (courseName === '') {
+    courseName = '-';
+  }
+  
+  const url = `${CLASSINFO_API_URL}/Classinfo/${academicYear}/${semester}/${campus}/-/${level}/${faculty}/${department}/${courseCode}/${courseName}/-/0`;
+  const res = await axiosInstance.get(url, authHeader);
+  return decodeBase64Response(res.data.result);
+};
+
+// export const gettokenservice = async () => {
+//   const res = await axios.get(TOKEN_API_URL);
+//   token = res.data;
+//   return res.data;
+// }
+
+const setStorage = (key: string, value: any) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+const getStorage = (key: string) => {
+  const value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : null;
+};
+
+const removeStorage = (key: string) => {
+  localStorage.removeItem(key);
+};
+
+const TOKEN_EXPIRY_SECONDS = 1800;
+
+const getTokenTime = (): number => {
+  return getStorage('tokentime') ? Number(getStorage('tokentime')) : 0;
+};
+
+const fetchNewToken = async (): Promise<string> => {
+  const res = await axios.get(TOKEN_API_URL);
+  // console.log('New token:', res.data.token);
+  const token = res.data.token;
+  setStorage('tokenweb', token);
+  setStorage('tokentime', Math.floor(Date.now() / 1000)); // Store current time in seconds
+  return token;
+};
+
+export const getToken = async (): Promise<string> => {
+  const token = getStorage('tokenweb');
+  const tokenTime = getTokenTime();
+  const currentTime = Math.floor(Date.now() / 1000);
+
+  if (!token || currentTime - tokenTime >= TOKEN_EXPIRY_SECONDS) {
+    return await fetchNewToken();
+  }
+
+  return token;
+};
+
+export const getUserInfo = async () => {
+  const token = await getToken();
+  const decoded = jwtDecode(token);
+  return decoded as any;
+}
+
+export const fetchDepartments = async (facultyId: string) => {
+  const authHeader = await getAuthHeader();
+  const url = `${DEPARTMENT_API_URL}/${facultyId}`;
+  const res = await axiosInstance.get(url, authHeader);
+  return decodeBase64Response(res.data.result);
+};
