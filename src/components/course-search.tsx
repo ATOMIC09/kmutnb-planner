f54ab24baec367570472076d9e146aa17d86dd44
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2024-11-19 21:57:11 +0700

    Add ภาคการศึกษา

--- src/components/course-search.tsx
+++ src/components/course-search.tsx
@@ -12,6 +12,7 @@ import {
 export default function CourseSearch() {
     const [year, setYear] = useState('');
     const [yearOptions, setYearOptions] = useState<string[]>([]);
+    const [semester, setSemester] = useState('1');
 
     useEffect(() => {
         // Get the current Gregorian year
@@ -28,28 +29,48 @@ export default function CourseSearch() {
     }, []);
 
     return (
-        <main className="px-12 py-4">
-            <div className="font-LINESeedSansTH_W_Rg text-gray-800 p-4 mt-4 mb-4 border-1 rounded-lg shadow-md">
-                <div className="text-2xl mb-4">วิชาที่เปิดสอน</div>
+        <main className="font-LINESeedSansTH_W_Rg text-gray-700 px-12 py-4">
+            <div className="text-4xl">วิชาที่เปิดสอน</div>
+            <div className="p-4 mt-4 mb-4 border-1 rounded-lg shadow-md">
+                <div className="mb-4">
+                    <div className="flex gap-8">
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">ปีการศึกษา</label>
+                            <Select 
+                                value={year} 
+                                onValueChange={(value) => setYear(value)}
+                            >
+                                <SelectTrigger className="w-[130px]">
+                                    <SelectValue defaultValue={year} placeholder="เลือกปีการศึกษา" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {yearOptions.map((option) => (
+                                        <SelectItem key={option} value={option}>
+                                            {option}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
 
-                <div className="flex-col items-center">
-                    <label className="block mb-2">ปีการศึกษา</label>
-                    <Select 
-                        value={year} 
-                        onValueChange={(value) => setYear(value)}
-                    >
-                        <SelectTrigger className="w-[130px]">
-                            <SelectValue defaultValue={year} placeholder="Select year" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {yearOptions.map((option) => (
-                                <SelectItem key={option} value={option}>
-                                    {option}
-                                </SelectItem>
-                            ))}
-                        </SelectContent>
-                    </Select>
-                </div>
+                        <div className="flex-col items-center">
+                            <label className="block mb-2">ภาคการศึกษา</label>
+                            <Select 
+                                value={semester} 
+                                onValueChange={(value) => setSemester(value)}
+                            >
+                                <SelectTrigger className="w-[130px]">
+                                    <SelectValue defaultValue={semester} placeholder="เลือกภาคการศึกษา" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="1">1</SelectItem>
+                                    <SelectItem value="2">2</SelectItem>
+                                    <SelectItem value="3">3</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </div>    
             </div>
         </main>
     );
